c3c1ab5f8ee2c8501cdbb94938246aa9905383ab
Cheprichate <petrovich15071998@mail.ru>
2023-09-28 20:53:27 +0400

    Добавьте файлы проекта.

---
+++ remains.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <iostream>
+int a;
+int b;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << " Введите первое число : ";
+    std::cin >> a ;
+    std::cout << " Введите второе число : ";
+    std::cin >> b ;
+    if (a % b == 0) {
+        std::cout << " Число " << a << " делится на " << b <<" без остатка.";
+    }
+    else {
+        std::cout << " Число " << a << " не делится на " << b << " без остатка.";
+    }
+}
